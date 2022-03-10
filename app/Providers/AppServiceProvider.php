75fa2687c13c2e8ff3427494282fa42d6b3953cd
Moses Kamau <moseskamau338@gmail.com>
2022-03-10 12:59:50 +0300

    About page done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('globalData', (object)[
+            'mainMenu'=>[
+                (object)['name' => 'Home', 'route' => 'home'],
+                (object)['name' => 'About Us', 'route' => 'about'],
+                (object)['name' => 'Contact Us', 'route' => 'contact'],
+                (object)['name' => 'News', 'route' => 'blog'],
+            ],
+            'members' => [
+                ['name'=>'Dr. Odhiambo Mbati','description'=>'Doctor', 'image'=>'images/team/Dr_Odhiambo_Bati.png'],
+                ['name'=>'Pricilla Okindo','description'=>'Doctor', 'image'=>'images/team/Priscilla_Okindo.png'],
+                ['name'=>'Ronnie Midigo','description'=>'Doctor', 'image'=>'images/team/Ronnie_Midigo.png'],
+                ['name'=>'Wilson Mosbey','description'=>'Doctor', 'image'=>'images/team/wilson_mosbey.png'],
+            ]
+        ]);
+
     }
 }
