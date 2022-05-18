b8b5b22e9598cac08d701759f71da4d7e66b9307
moses kamau <moseskamau338@gmail.com>
2022-05-18 19:36:00 +0300

    slider complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,12 @@
 import Alpine from 'alpinejs'
 Alpine.data('events', () => ({
-     skip: 4,
+     skip: 1,
     slider: null,
     active: 1,
     total: null,
     interval: 3000,
     autoplay: false,
     direction: 'right',
-    events:[
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-        {title:'Home Care Seminar', description:'How do give your best in caring for a sick loved one at home?', date:'12th June, 2022', venue:'Nairobi', image:'/images/care.jpg', category:'Training'},
-    ],
-    init() {
-
-        //this.$nextTick(() => {
-        //    this.total = this.$refs.slider.children.length;
-        //});
-
-        if(this.autoplay) {
-            this.play();
-        }
-    },
     next() {
         this.to((current, offset) => current + (offset * this.skip))
     },
@@ -35,36 +16,11 @@ Alpine.data('events', () => ({
 
     to(strategy) {
         let slider = this.$refs.slider
+        window.slider = slider
         let current = slider.scrollLeft
         let offset = slider.firstElementChild.getBoundingClientRect().width
 
         slider.scrollTo({ left: strategy(current, offset), behavior: 'smooth' })
-    },
-    play() {
-        let counter = this.active;
-        // run every this.interval milliseconds
-        let interval = setInterval(() => {
-            // check if direction is right and click next
-            if(this.direction === 'right') {
-                this.next();
-                counter++;
-            }
-            // check if direction is left and click prev
-            if(this.direction === 'left') {
-                this.prev();
-                counter--;
-            }
-            // check if counter is equal to total and change direction to left
-            if(counter === this.total) {
-                this.direction = 'left';
-            }
-            // check if counter is equal to 1 and change direction to right
-            if (counter === this.active) {
-                this.direction = 'right';
-            }
-
-        }, this.interval);
-
     },
     focusableWhenVisible: {
         'x-intersect:enter'() {
--- resources/views/layouts/partials/events.blade.php
+++ resources/views/layouts/partials/events.blade.php
@@ -8,24 +8,24 @@
 
         <div id="wrapper" x-data="events">
             <div @keydown.right="next" @keydown.left="prev" x-ref="slider" class="flex items-center overflow-x-auto pb-10 mt-8 mb-2 sm:mt-16 space-x-2 snap-x snap-mandatory">
-                <template x-for="(event, index) in events">
+                @foreach($globalData->events as $event)
                     <div class="relative flex flex-col flex-shrink-0 items-start justify-end col-span-12 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4 w-[350px]">
-                        <a href="#_" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-48 hover:scale-110" style="background-image:url('/images/care.jpg')">
+                        <a href="#_" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-48 hover:scale-110" style="background-image:url('{{$event->image}}')">
                         </a>
-                        <div class="relative z-20 w-full h-auto py-8 text-white bg-white bg-gradient-to-br from-cyan-900 via-cyan-800 to-pink-400 border-t-0 border-yellow-200 px-7">
-                            <a href="#_" class="inline-block text-xs font-semibold absolute top-0 -mt-3.5 rounded-full px-4 py-2 uppercase text-purple-500 bg-white" x-text="event.category"></a>
-                            <h2 class="mb-5 text-2xl font-bold"><a href="#_" x-text="event.title"></a></h2>
-                            <p class="mb-2 text-lg font-normal text-purple-100 opacity-100" x-text="event.description"></p>
+                        <div class="relative z-50 w-full h-64 overflow-hidden py-8 text-white bg-white bg-gradient-to-br from-cyan-900 via-cyan-800 to-pink-400 border-t-0 border-yellow-200 px-7">
+
+                            <h2 class="mb-5 text-2xl font-bold"><a href="#_">{{\Str::limit($event->title, 25)}}</a></h2>
+                            <p class="mb-2 text-lg font-normal text-purple-100 opacity-100">{{$event->description}}</p>
 
                         </div>
                     </div>
-                </template>
+                @endforeach
 
 
             </div>
             {{--buttons--}}
-            <button @click="prev" class="px-3 py-1 bg-green-500 rounded-sm">&leftarrow;</button>
-            <button @click="next" class="px-3 py-1 bg-green-500 rounded-sm">&rightarrow;</button>
+            <button @click="prev" class="px-3 py-1 bg-gray-700 text-white rounded-sm hover:bg-gray-800 focus:ring focus:ring-sky-600 rounded-sm">&leftarrow;</button>
+            <button @click="next" class="px-3 py-1 bg-gray-700 text-white rounded-sm hover:bg-gray-800 focus:ring focus:ring-sky-600 rounded-sm">&rightarrow;</button>
         </div>
     </div>
 </section>
